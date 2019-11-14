e2b989d5f907be463b6cfa56c10be0add14b48cb
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2019-11-14 09:31:50 +0700

    add success custom camera with surface view, but have code deprecated

---
+++ app/src/main/java/com/rifafauzi/customcamerasurfaceview/CameraPreview.kt
@@ -0,0 +1,79 @@
+package com.rifafauzi.customcamerasurfaceview
+
+import java.io.IOException
+import android.content.Context
+import android.hardware.Camera
+import android.util.Log
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.view.View
+
+/**
+ * Created by rrifafauzikomara on 2019-11-14.
+ */
+
+class CameraPreview(context: Context, private var mCamera: Camera?) : SurfaceView(context), SurfaceHolder.Callback {
+    private val mHolder: SurfaceHolder
+
+    init {
+        mHolder = holder
+        mHolder.addCallback(this)
+        // deprecated setting, but required on Android versions prior to 3.0
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        try {
+            // create the surface and start camera preview
+            if (mCamera == null) {
+                mCamera!!.setPreviewDisplay(holder)
+                mCamera!!.startPreview()
+            }
+        } catch (e: IOException) {
+            Log.d(View.VIEW_LOG_TAG, "Error setting camera preview: " + e.message)
+        }
+
+    }
+
+    fun refreshCamera(camera: Camera?) {
+        if (mHolder.surface == null) {
+            // preview surface does not exist
+            return
+        }
+        // stop preview before making changes
+        try {
+            mCamera!!.stopPreview()
+        } catch (e: Exception) {
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+        // start preview with new settings
+        setCamera(camera)
+        try {
+            mCamera!!.setPreviewDisplay(mHolder)
+            mCamera!!.startPreview()
+        } catch (e: Exception) {
+            Log.d(View.VIEW_LOG_TAG, "Error starting camera preview: " + e.message)
+        }
+
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, w: Int, h: Int) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+        refreshCamera(mCamera)
+    }
+
+    fun setCamera(camera: Camera?) {
+        //method to set a camera instance
+        mCamera = camera
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        // TODO Auto-generated method stub
+        // mCamera.release();
+
+    }
+}
--- app/src/main/java/com/rifafauzi/customcamerasurfaceview/MainActivity.kt
+++ app/src/main/java/com/rifafauzi/customcamerasurfaceview/MainActivity.kt
@@ -1,12 +1,59 @@
 package com.rifafauzi.customcamerasurfaceview
 
+import android.Manifest
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import android.widget.Toast
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val builder = StrictMode.VmPolicy.Builder()
+        StrictMode.setVmPolicy(builder.build())
+
+        requestReadPermissions()
+
     }
+
+    private fun requestReadPermissions() {
+        Dexter.withActivity(this)
+            .withPermissions( Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA)
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport) {
+                    // check if all permissions are granted
+                    if (report.areAllPermissionsGranted()) {
+                        Toast.makeText(applicationContext, "All permissions are granted by user!", Toast.LENGTH_SHORT)
+                            .show()
+                        val intent = Intent(this@MainActivity, CaptureActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+
+                    // check for permanent denial of any permission
+                    if (report.isAnyPermissionPermanentlyDenied) {
+                        // show alert dialog navigating to Settings
+                        //openSettingsDialog();
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(permissions: List<PermissionRequest>, token: PermissionToken) {
+                    token.continuePermissionRequest()
+                }
+            }).withErrorListener { Toast.makeText(applicationContext, "Some Error! ", Toast.LENGTH_SHORT).show() }
+            .onSameThread()
+            .check()
+    }
+
 }
