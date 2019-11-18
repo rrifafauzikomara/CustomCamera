c959b3878b90bd11c2fd5f4c5b3cfd11e226ec9d
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2019-11-18 16:02:28 +0700

    add fix recognition text using ml kit

--- app/src/main/java/com/rifafauzi/customcamerasurfaceview/MainActivity.kt
+++ app/src/main/java/com/rifafauzi/customcamerasurfaceview/MainActivity.kt
@@ -5,11 +5,19 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Matrix
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.firebase.FirebaseApp
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.text.FirebaseVisionText
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
@@ -20,11 +28,14 @@ class MainActivity : AppCompatActivity() {
 
     private val imageView by lazy { findViewById<ImageView>(R.id.text_recognition_image_view) }
     private val btnCamera by lazy { findViewById<Button>(R.id.btnCamera) }
-    private val btnProcess by lazy { findViewById<Button>(R.id.btnRecognition) }
+    private val btnProcess by lazy { findViewById<FrameLayout>(R.id.btnRecognition) }
+    private val recyclerView by lazy { findViewById<RecyclerView>(R.id.recycler_view) }
+    private val textRecognitionModels = ArrayList<TextModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FirebaseApp.initializeApp(this)
 
         val bitmap = CameraActivity.bitmap
         val rotatedBitmap = bitmap?.rotate(90)
@@ -36,8 +47,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnProcess.setOnClickListener {
-
+            analyzeImage(rotatedBitmap)
         }
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = TextAdapter(this, textRecognitionModels)
     }
 
     private fun Bitmap.rotate(degree:Int):Bitmap{
@@ -96,6 +110,74 @@ class MainActivity : AppCompatActivity() {
             }).withErrorListener { Toast.makeText(applicationContext, "Some Error! ", Toast.LENGTH_SHORT).show() }
             .onSameThread()
             .check()
+    }
+
+    private fun analyzeImage(image: Bitmap?) {
+        if (image == null) {
+            Toast.makeText(this, "There was some error", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        imageView.setImageBitmap(null)
+        textRecognitionModels.clear()
+        recyclerView.adapter?.notifyDataSetChanged()
+        showProgress()
+
+        val firebaseVisionImage = FirebaseVisionImage.fromBitmap(image)
+        val textRecognizer = FirebaseVision.getInstance().onDeviceTextRecognizer
+        textRecognizer.processImage(firebaseVisionImage)
+            .addOnSuccessListener {
+                val mutableImage = image.copy(Bitmap.Config.ARGB_8888, true)
+
+                recognizeText(it, mutableImage)
+
+                imageView.setImageBitmap(mutableImage)
+                hideProgress()
+                recyclerView.adapter?.notifyDataSetChanged()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "There was some error", Toast.LENGTH_SHORT).show()
+                hideProgress()
+            }
+    }
+
+
+    private fun recognizeText(result: FirebaseVisionText?, image: Bitmap?) {
+
+        if (result == null || image == null) {
+            return Toast.makeText(this, "There was some error", Toast.LENGTH_SHORT).show()
+        } else if (result.textBlocks.size == 0) {
+            Toast.makeText(this,"Gunakan landscape mode agar teks bisa terbaca", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+//        val canvas = Canvas(image)
+//        val rectPaint = Paint()
+//        rectPaint.color = Color.RED
+//        rectPaint.style = Paint.Style.STROKE
+//        rectPaint.strokeWidth = 4F
+//        val textPaint = Paint()
+//        textPaint.color = Color.RED
+//        textPaint.textSize = 40F
+
+        var index = 0
+        for (block in result.textBlocks) {
+            for (line in block.lines) {
+//                canvas.drawRect(line.boundingBox!!, rectPaint)
+//                canvas.drawText(index.toString(), line.cornerPoints!![2].x.toFloat(), line.cornerPoints!![2].y.toFloat(), textPaint)
+                textRecognitionModels.add(TextModel(index++, line.text))
+            }
+        }
+    }
+
+    private fun showProgress() {
+        findViewById<View>(R.id.btnText).visibility = View.GONE
+        findViewById<View>(R.id.btnProgress).visibility = View.VISIBLE
+    }
+
+    private fun hideProgress() {
+        findViewById<View>(R.id.btnText).visibility = View.VISIBLE
+        findViewById<View>(R.id.btnProgress).visibility = View.GONE
     }
 
 }
